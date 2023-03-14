3280db7c6a3fe4fa4aa1603925d0cf8dc653ad17
Mahmoud Rifaat <mahmoud.ref2at.dev@gmail.com>
2023-03-14 13:11:44 +0200

    Adding JS-1 task1 solution

--- 2. Javascript/JS-1/script.js
+++
@@ -1,8 +0,0 @@
-let i;
-for(i = 0; i < 6; ++i){
-    const message = prompt("enter message number " + (i+1) + ": ");
-    const messageElement = document.createElement("h" + String(i+1));
-    console.log(messageElement);
-    messageElement.innerHTML = message;
-    document.appendChild(messageElement);
-}
---
+++ 2. Javascript/JS-1/task1-script.js
@@ -0,0 +1,11 @@
+const userSection = document.getElementById('user-output');
+let i;
+const headings = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+for(i = 1; i <= 6; ++i){
+    const message = prompt("enter message number " + i + ": ");
+    const headingElement = document.createElement(headings[i-1]);
+    console.log(message);
+    console.log(headingElement);
+    headingElement.textContent = message;
+    userSection.appendChild(headingElement);
+}
